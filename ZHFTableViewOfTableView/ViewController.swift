1ae8a86675928390c96ee02c3a9be68e8f08ecc7
张海峰 <991150443@qq.com>
2018-05-14 09:34:10 +0800

    Update ViewController.swift

--- ZHFTableViewOfTableView/ViewController.swift
+++ ZHFTableViewOfTableView/ViewController.swift
@@ -229,6 +229,11 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
         }
         
     }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view :UIView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: ScreenWidth, height: 0.5))
+        view.backgroundColor = UIColor.white
+        return view
+    }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 0.5
     }
